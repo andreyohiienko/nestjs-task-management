5cf00b64fef13a14754602027fb08d110110ea02
Andrey Ohiienko <andreyohiienko@gmail.com>
2020-10-27 13:03:42 +0200

    Validation authCredentialsDto, password strength

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
-import { AuthCredentialsDto } from './dto/auth-credentials'
+import { AuthCredentialsDto } from './dto/auth-credentials.dto'
 import { UserRepository } from './user.repositoty'
 
 @Injectable()
